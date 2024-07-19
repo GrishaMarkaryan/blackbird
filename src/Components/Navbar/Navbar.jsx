3e25fc54aa89a07975d1039643263f532d0a5cbf
GrishaMarkaryan <grisha-markaryan@mail.ru>
2024-07-19 20:40:20 +0300

    removed rebundant nested blocks

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,70 +1,68 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
-import dropdown_icon1 from '../Assets/dropdown_icon1.png'
+import dropdown_icon1 from '../Assets/dropdown_icon1.png';
 
 const Navbar = () => {
     const [menu, setMenu] = useState('shop');
-    const {getTotalCartItems} = useContext(ShopContext);
+    const { getTotalCartItems } = useContext(ShopContext);
     const menuRef = useRef();
     const dropdownRef = useRef();
-    
+
     const dropdown_toggle = (e) => {
         menuRef.current.classList.toggle('nav-menu-visible');
-        e.target.classList.toggle('open');       {/* e - это стрелка, на который нажимаем */}
+        e.target.classList.toggle('open'); // e - это стрелка, на который нажимаем
     }
 
     const closeDropdown = () => {
         if (menuRef.current.classList.contains('nav-menu-visible')) {
             menuRef.current.classList.remove('nav-menu-visible');
-            dropdownRef.current.classList.remove('open')
+            dropdownRef.current.classList.remove('open');
         }
     }
 
     useEffect(() => {
         const handler = (e) => {
             if (menuRef.current && !menuRef.current.contains(e.target) && !dropdownRef.current.contains(e.target)) {
-                closeDropdown()
+                closeDropdown();
             }
         }
-            document.addEventListener('mousedown', handler);
-            return () => {
-                document.removeEventListener('mousedown', handler)
-            }
-        }, [])
-    
-        const handleMenuClick = (menuName) => {
-            setMenu(menuName);
-            closeDropdown()
+        document.addEventListener('mousedown', handler);
+        return () => {
+            document.removeEventListener('mousedown', handler);
         }
-   
+    }, []);
+
+    const handleMenuClick = (menuName) => {
+        setMenu(menuName);
+        closeDropdown();
+    }
 
     return (
         <div className="navbar">
             <Link to='/' className="nav">
                 <div className="nav_logo">
-                    <img src={logo} alt='' className="logo"/>
-                    <p>  BLACKBIRD </p>
+                    <img src={logo} alt='' className="logo" />
+                    <p>BLACKBIRD</p>
                 </div>
             </Link>
-
             <img ref={dropdownRef} className="nav-dropdown" onClick={dropdown_toggle} src={dropdown_icon1} alt="" />
             <ul ref={menuRef} className="nav-menu">
-                <li onClick={()=> {handleMenuClick('shop')}}> <Link style={{textDecoration: 'none'}} to='/'> Shop </Link> {menu ==='shop'? <hr/>: <></>} </li>
-                <li onClick={()=> {handleMenuClick('mens')}}> <Link style={{textDecoration: 'none'}} to='/men'> Men </Link> {menu ==='mens'? <hr/> : <></>}</li>
-                <li onClick={()=> {handleMenuClick('womens')}}> <Link style ={{textDecoration: 'none'}} to='/women'> Women </Link> {menu === 'womens'? <hr/>: <></>} </li>
-                <li onClick={()=> {handleMenuClick('kids')}}> <Link style={{textDecoration: 'none'}} to='/kids'> Kids </Link> {menu === 'kids'? <hr/>: <></>}</li>
+                <li onClick={() => { handleMenuClick('shop') }}> <Link style={{ textDecoration: 'none' }} to='/'> Shop </Link> {menu === 'shop' ? <hr /> : <></>} </li>
+                <li onClick={() => { handleMenuClick('mens') }}> <Link style={{ textDecoration: 'none' }} to='/men'> Men </Link> {menu === 'mens' ? <hr /> : <></>}</li>
+                <li onClick={() => { handleMenuClick('womens') }}> <Link style={{ textDecoration: 'none' }} to='/women'> Women </Link> {menu === 'womens' ? <hr /> : <></>} </li>
+                <li onClick={() => { handleMenuClick('kids') }}> <Link style={{ textDecoration: 'none' }} to='/kids'> Kids </Link> {menu === 'kids' ? <hr /> : <></>} </li>
             </ul>
             <div className="nav_login_cart">
                 <Link to='/login'> <button> Login </button> </Link>
-                <Link to='/cart'> <img src={cart_icon} alt=''/></Link>
+                <Link to='/cart'> <img src={cart_icon} alt='' /></Link>
                 <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -5,57 +5,51 @@ import { createContext } from "react";
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {}
-    for (let index = 0; index < all_product.length+1; index++) {
+    let cart = {};
+    for (let index = 0; index < all_product.length + 1; index++) {
         cart[index] = 0;
     }
-return cart;       {/* здесь возвращаем cart - объект, в котором значения всех ключей равны нулю:   0: 0, 1: 0, 2: 0, 3: 0 ... 36: 0}  */}
+    return cart; // здесь возвращаем cart - объект, в котором значения всех ключей равны нулю: 0: 0, 1: 0, 2: 0, 3: 0 ... 36: 0
 }
 
 const ShopContextProvider = (props) => {
-    
-    const [cartItems, setCartItems] = useState(getDefaultCart())    
-       {/* т.к. getDefaultCart() возвращает cart, то cartItems принимает значение cart */} 
+    const [cartItems, setCartItems] = useState(getDefaultCart()); // т.к. getDefaultCart() возвращает cart, то cartItems принимает значение cart
 
     const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1 }))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
 
     const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1 }))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     }
 
-
     const getTotalCartAmount = () => {
-        let totalAmount = 0;                 {/*это функция показывает общую сумма товаров в корзине */}
-        for (const item in cartItems)
-        {
-            if(cartItems[item]>0)
-            {
-                let itemInfo = all_product.find((product)=> product.id===Number(item))
+        let totalAmount = 0; // это функция показывает общую сумма товаров в корзине
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item));
                 totalAmount += itemInfo.new_price * cartItems[item];
-            }     
-        } 
-        return totalAmount; 
+            }
+        }
+        return totalAmount;
     }
 
     const getTotalCartItems = () => {
-        let totalItem = 0;
-        for(const item in cartItems) {        {/*это функция показывает количество товаров на иконке cart сверху справа */}
-            if(cartItems[item]>0) {
+        let totalItem = 0; // это функция показывает количество товаров на иконке cart сверху справа
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
                 totalItem += cartItems[item];
             }
         }
         return totalItem;
     }
 
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems}
+    const contextValue = { all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems };
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
-    )
-} 
-
+    );
+}
 
 export default ShopContextProvider;
